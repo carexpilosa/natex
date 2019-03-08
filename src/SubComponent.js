05ec89344e12b970c5290bc03132fe632ff3c8c0
carexpilosa <chicoantares@web.de>
2019-03-08 19:22:42 +0100

    naturex added

---
+++ src/SubComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class SubComponent extends React.Component {
+  render() {
+    return <h3>SubComponent</h3>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SubComponent from './SubComponent';
+
+ReactDOM.render(
+  <div>
+    <h1>*** Roller ***</h1>
+    <SubComponent />
+  </div>,
+  document.getElementById('root')
+);

86bc6e232c7316c0e3d87b8c6b1ffecb195cab49
carexpilosa <chicoantares@web.de>
2019-03-09 12:05:54 +0100

    dev: rendering Menu

--- src/Menu.js
+++ src/Menu.js
@@ -13,43 +13,8 @@ class Menu extends React.Component {
   render() {
     return (
       <div>
-        <h3>Menu</h3>
-        <div id='mainMenu'>
-          mainMenu
-          <div>
-            <Link
-              onMouseOver={e => this._onMouseOver(e)}
-              onMouseOut={e => this._onMouseOut(e)}
-              to='/elinks'
-            >
-              eventLinks
-            </Link>
-            &nbsp;
-            <Link
-              onMouseOver={e => this._onMouseOver(e)}
-              onMouseOut={e => this._onMouseOut(e)}
-              to='/event1'
-            >
-              event 1
-            </Link>
-            &nbsp;
-            <Link
-              onMouseOver={e => this._onMouseOver(e)}
-              onMouseOut={e => this._onMouseOut(e)}
-              to='/'
-            >
-              heem
-            </Link>
-          </div>
-          <button
-            onClick={() =>
-              this.setState({ subVisible: !this.state.subVisible })
-            }
-          >
-            toggleSub
-          </button>
-        </div>
-        {this.state.subVisible && <div id='subMenu'>subMenu</div>}
+        {this.renderMainMenu()}
+        {this.renderSubMenu()}
       </div>
     );
   }
@@ -60,6 +25,52 @@ class Menu extends React.Component {
 
   _onMouseOut(e) {
     e.target.style.backgroundColor = '';
+  }
+
+  renderMainMenu() {
+    return (
+      <div id='mainMenu'>
+        <h4>
+          mainMenu&nbsp;
+          <button
+            onClick={() =>
+              this.setState({ subVisible: !this.state.subVisible })
+            }
+          >
+            toggleSub
+          </button>
+        </h4>
+        <div>
+          <Link
+            onMouseOver={e => this._onMouseOver(e)}
+            onMouseOut={e => this._onMouseOut(e)}
+            to='/elinks'
+          >
+            eventLinks
+          </Link>
+          &nbsp;
+          <Link
+            onMouseOver={e => this._onMouseOver(e)}
+            onMouseOut={e => this._onMouseOut(e)}
+            to='/event1'
+          >
+            event 1
+          </Link>
+          &nbsp;
+          <Link
+            onMouseOver={e => this._onMouseOver(e)}
+            onMouseOut={e => this._onMouseOut(e)}
+            to='/'
+          >
+            heem
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  renderSubMenu() {
+    return <div id='subMenu'>{this.state.subVisible && <h5>subMenu</h5>}</div>;
   }
 }
 

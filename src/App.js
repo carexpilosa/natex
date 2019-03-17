a99208cd906038594c7b79645462d5de26c83ea8
carexpilosa <chicoantares@web.de>
2019-03-17 15:01:03 +0100

    dev: restructuring components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//import Menu from './Menu';
+import Menu from './Menu';
 //import EventLinks from './EventLinks';
 import Event from './Event';
 //import Header from './Header';
@@ -8,14 +8,30 @@ import eventData from './eventData';
 
 import './styles.css';
 
+const Home = () => {
+  return <h5>HOME</h5>;
+};
+
 class App extends React.Component {
   render() {
     {
-      return eventData.map((event, idx) => {
-        return <Event key={idx} data={event} />;
-      });
+      return (
+        <Router basename='/wandern'>
+          <React.Fragment>
+            <Route path='/' component={Menu} />
+            <Switch>
+              {eventData.map((event, idx) => {
+                return (
+                  <Route key={idx} path={event.path}>
+                    <Event data={event} />
+                  </Route>
+                );
+              })}
+            </Switch>
+          </React.Fragment>
+        </Router>
+      );
     }
-    //return <div>HEllo</div>;
   }
   /*render() {
     return (
--- src/Event.js
+++ src/Event.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import image from './img/hellbach.jpeg';
 
 class Event extends React.Component {
   render() {
-    const { title, date, contentHTML, img } = this.props.data;
+    const { title, date, contentHTML, image } = this.props.data;
+    const imgSrc = require(`./img/${image}`);
     return (
       <div>
         <h3>{title}</h3>
         <h4>{date}</h4>
         <div dangerouslySetInnerHTML={{ __html: contentHTML }} />
         <div id='rightContent'>
-          <img id='rightImg' src={image} alt='hellbach' />
+          <img id='rightImg' src={imgSrc} alt='hellbach' />
         </div>
       </div>
     );

4ef4db3335df109cdf1a5edc3adcc3df008e53b2
carexpilosa <chicoantares@web.de>
2019-03-17 14:12:57 +0100

    img in dev

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import EventLinks from './EventLinks';
 import Event1 from './Event1';
 import Header from './Header';
 import eventData from './eventData';
+import image from './img/hellbach.jpeg';
 import './styles.css';
 
 class App extends React.Component {
@@ -48,7 +49,21 @@ class App extends React.Component {
             </Switch>
           </div>
           <div id='rightContent'>
-            <img id='rightImg' src='./img/hellbach.jpeg' alt='hellbach' />
+            <img id='rightImg' src={image} alt='hellbach' />
+            {/*<Switch>
+              <Route
+                path='/'
+                render={() => {
+                  return (
+                    <div>
+                      {eventData.map((event, idx) => {
+                        return <span key={`k${idx}`}>bla</span>;
+                      })}
+                    </div>
+                  );
+                }}
+              />
+              </Switch>*/}
           </div>
         </React.Fragment>
       </Router>
